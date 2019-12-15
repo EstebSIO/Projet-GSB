985cdeb8d0c8bb347a2f7ae033f1965ade339f27
JulienCasta <juliencasta16@gmail.com>
2019-12-15 17:14:16 +0100

    Ajax

--- js/remplirMois.js
+++ js/remplirMois.js
@@ -1,20 +1,43 @@
-    var visiteur = document.getElementById("lstVisiteur");
-    visiteur.addEventListener('change', function() {
-          hydrater();
-      });
+var visiteur = document.getElementById("lstVisiteur");
+ visiteur.addEventListener('change', function() {
+       hydrater();
+   });
 
-    function hydrater(){  
-        var idVisiteur = document.getElementById("lstVisiteur").value;
-        var lstMois = document.getElementById("lstMois");
-        lstMois.innerHTML='';
-        var Mois = [Math.random(), Math.random(), Math.random()]
+ function hydrater(){  
+     //var data={remplirMois:Mois};
+     var Mois = $(this).val();
+     var idVisiteur = document.getElementById("lstVisiteur").value;
+     var lstMois = document.getElementById("lstMois");
+     lstMois.innerHTML='';
+     $.ajax({
+         type : "POST",
+         url: 'http://gsb/?uc=valider&action=selectionnerVisiteur',
+         data: idVisiteur,
+         sucess : function(){
+             console.log('gg');
+         },
+         error : function(){
+             console.log('erreur');
+         }
+     });
+     /*$.ajax({
+         type : "POST",
+         url: '../js.getMois.php',
+         data: data,
+         sucess : function(){
+             console.log('succès');
+         },
+         error : function(){
+             console.log('erreurPHP');
+         }
+     });
+       */
 
-
-        for(var i = 0; i < Mois.length; i++) {
-            var opt = Mois[i];
-            var el = document.createElement("option");
-            el.textContent = opt;
-            el.value = opt;
-            lstMois.appendChild(el);
-        }
-    }
+     for(var i = 0; i < Mois.length; i++) {
+         var opt = Mois[i];
+         var el = document.createElement("option");
+         el.textContent = opt;
+         el.value = opt;
+         lstMois.appendChild(el);
+     }
+ }
--- vues/v_enteteComptable.php
+++ vues/v_enteteComptable.php
@@ -25,6 +25,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="./styles/bootstrap/bootstrap.css" rel="stylesheet">
         <link href="./styles/style.css" rel="stylesheet">
+        <script src="http://code.jquery.com/jquery.js" type="text/javascript"></script>
     </head>
     <body>
         <div class="container">
